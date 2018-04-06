80b94b70044b445bf1f6aeece3e04d555dedc559
Tirod <37984237+Tirod@users.noreply.github.com>
2018-04-06 19:31:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 const Discord = require('discord.js')
-const bot = new Discord.Client()
-
+const bot = new Discord.Client() 
 
 
 bot.on('message', function (message) {
-if (message.content === '!chaine T') {
-message.reply('TiRoD:https://www.youtube.com/channel/UCoHJK4-WCp4zKWovFE8hhSw?view_as=subscriber ')
-}
+  if (message.content === '!chaine T') {
+   message.reply('TiRoD:https://www.youtube.com/channel/UCoHJK4-WCp4zKWovFE8hhSw?view_as=subscriber ')
+  }
 })
 
 
 bot.on('message', function (message) {
-if (message.content === '!chaine F') {
-message.reply('Frizoux2.0: https://www.youtube.com/channel/UCnSYBQsAKlt7XXnR0f8JSmw ')
-}
+  if (message.content === '!help') {
+   message.reply('Alors les commande:!chaine T: chaine de TiRoD ')
+  }
 })
 
 
 bot.on('message', function (message) {
-if (message.content === '!help') {
-message.reply('Alors les commande: !chaine F :chaine de Frizoux2.0, !chaine T: chaine de TiRoD ')
-}
+  if (message.content === '!régle') {
+   message.reply('#1:Ne pas insulte, #2:Ne pas spawm, #3:Ne pas mal parler')
+  }
 })
 
 
 bot.on('message', function (message) {
-if (message.content === '!régle') {
-message.reply('#1:Ne pas insulte, #2:Ne pas spawm, #3:Ne pas mal parler')
-}
+  if (message.content === '!Form') {
+   message.reply('INDISPONIBLE')
+  }
 })
 
 
 bot.on('message', function (message) {
-if (message.content === '!Form') {
-message.reply('INDISPONIBLE')
-}
+  if (message.content === '!Info TB') {
+   message.reply('Le Tribots(TB) est en constrution. Le bots pourra faire sortir vos musique préférè...')
+  }
 })
 
-bot.login('process.env.TOKEN')
+
+bot.login('NDI4MjQ0NTM5ODQ1NzcxMjY0.DaZPYg.SGPPTF4pbfCM9Evd27ORcgFiibw')
